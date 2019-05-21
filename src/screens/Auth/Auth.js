e710846e37b08e4d5076653476a16f1cdb22e9d2
rochafialvin <rochafi.dev@gmail.com>
2019-05-21 09:36:36 +0700

    Switching Auth Screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -9,28 +9,68 @@ import imageBackground from '../../assets/white-cat.jpg'
 import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground'
 
 class AuthScreen extends Component {
+    state = {
+        authMode : 'login'
+    }
+
     loginHandler= () => {
         startMainTabs()
     }
 
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {
+                authMode: prevState.authMode === 'login' ? 'signup' : 'login'
+            }
+        })
+    }
+
     render () {
+        let submitButtonControl, headingTextControl
+        let confirmPasswordControl = null
+
+        if(this.state.authMode === 'signup'){
+            confirmPasswordControl = (
+                <DefaultInput placeholder='Confirm Password'/>
+            )
+            submitButtonControl = (
+                <ButtonWithBackground color='#a5b4ef' onTekan={this.loginHandler}>
+                    Signup
+                </ButtonWithBackground>
+            )
+
+            headingTextControl = (
+                <MainText>
+                    <HeadingText>Please Sign Up</HeadingText>
+                </MainText>
+            )
+        } else {
+            submitButtonControl = (
+                <ButtonWithBackground color='#a5b4ef' onTekan={this.loginHandler}>
+                    Login
+                </ButtonWithBackground>
+            )
+
+            headingTextControl = (
+                <MainText>
+                    <HeadingText>Please Log In</HeadingText>
+                </MainText>
+            )
+        }
+
         return (
             <ImageBackground source={imageBackground} style={styles.backgroundImage}>
                 <View style={styles.container}>
-                    <MainText>
-                        <HeadingText>Please Log In</HeadingText>
-                    </MainText>
-                    <ButtonWithBackground color='#a5b4ef'>
-                        Switch to Login
+                    {headingTextControl}
+                    <ButtonWithBackground color='#a5b4ef' onTekan={this.switchAuthModeHandler}>
+                        Switch to {this.state.authMode === 'login' ? 'Signup' : 'Login'}
                     </ButtonWithBackground>
                     <View style={styles.inputContainer}>
                         <DefaultInput placeholder='Your E-Mail Address' style/>
                         <DefaultInput placeholder='Password'/>
-                        <DefaultInput placeholder='Confirm Password'/>
+                        {confirmPasswordControl}
                     </View>
-                    <ButtonWithBackground color='#a5b4ef' onTekan={this.loginHandler}>
-                        Login
-                    </ButtonWithBackground>
+                    {submitButtonControl}
                 </View>
             </ImageBackground>
         )
