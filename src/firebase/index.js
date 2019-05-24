7791bab2ba89c338fcab29c6f69595b282b0461c
tomirp <tomirpdevelopment@gmail.com>
2019-05-24 09:19:08 +0700

    add firebase for database

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
-
-var firebaseConfig = {
-    apiKey: "AIzaSyBGIDx6Y4MRrg-g_7_7VTEXJ-mVvM8rgro",
-    authDomain: "jc8ntapnative.firebaseapp.com",
-    databaseURL: "https://jc8ntapnative.firebaseio.com",
-    projectId: "jc8ntapnative",
-    storageBucket: "jc8ntapnative.appspot.com",
-    messagingSenderId: "397247298348",
-    appId: "1:397247298348:web:c18577bdf14a434e"
-  };
+ // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyAi_xIJMA_JPWgZ7Medl9yN1b_fDk6qZ8M",
+      authDomain: "projectmobilesaya.firebaseapp.com",
+      databaseURL: "https://projectmobilesaya.firebaseio.com",
+      projectId: "projectmobilesaya",
+      storageBucket: "projectmobilesaya.appspot.com",
+      messagingSenderId: "553503129392",
+      appId: "1:553503129392:web:4179fd8e5c831e19"
+    
+  }
   // Initialize Firebase
   export const Fire = firebase.initializeApp(firebaseConfig);
